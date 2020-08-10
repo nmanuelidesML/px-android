cc3d74305b1ad6f9ce95ba0025932f270bdd89f5
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-08-10 12:31:10 -0300

    scrore added in mock

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/features/payment_congrats/PaymentCongratsMock.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/features/payment_congrats/PaymentCongratsMock.java
@@ -9,20 +9,28 @@ import java.util.ArrayList;
 public class PaymentCongratsMock {
 
     public static PaymentCongratsModel getMock() {
-        ArrayList<PaymentInfo> paymentList = new ArrayList();
 
+        // Discounts
         ArrayList<PaymentCongratsResponse.Discount.Item> itemList = new ArrayList();
         for (int i = 0; i <= 8; i++) {
             PaymentCongratsResponse.Discount.Item discountItem = new PaymentCongratsResponse.Discount.Item("Hasta","20% OFF","https://mla-s1-p.mlstatic.com/952848-MLA41109062105_032020-O.jpg", "mercadopago://discount_center_payers/detail?campaign_id\\u003d1048784\\u0026user_level\\u003d3\\u0026mcc\\u003d561013\\u0026distance\\u003d256\\u0026coupon_used\\u003dfalse\\u0026status\\u003dFULL\\u0026store_id\\u003d30188107\\u0026sections\\u003d%5B%7B%22id%22%3A%22header%22%2C%22type%22%3A%22header%22%2C%22content%22%3A%7B%22logo%22%3A%22https%3A%2F%2Fmla-s1-p.mlstatic.com%2F952848-MLA41109062105_032020-O.jpg%22%2C%22title%22%3A%2220%25%20OFF%22%2C%22subtitle%22%3A%22El%20Noble%22%2C%22level%22%3A%7B%22icon%22%3A%22discount_payers_black_check%22%2C%22label%22%3A%22NIVEL%201%22%2C%22format%22%3A%7B%22text_color%22%3A%22%23000000%22%2C%22background_color%22%3A%22%23EDEDED%22%7D%7D%7D%7D%5D#from\\u003d/px/congrats","1048784");
             itemList.add(discountItem);
         }
-
         PaymentCongratsResponse.Action action = new PaymentCongratsResponse
             .Action("Ver todos los descuentos", "mercadopago://discount_center_payers/list#from\\u003d/px/congrats");
         PaymentCongratsResponse.Discount.DownloadApp downloadApp =
             new PaymentCongratsResponse.Discount.DownloadApp("Exclusivo con la app de Mercado Pago", action);
         PaymentCongratsResponse.Discount discount =
             new PaymentCongratsResponse.Discount("Descuentos por tu nivel", "", action, downloadApp, null, itemList);
+
+        //Score
+        PaymentCongratsResponse.Score.Progress progress =
+            new PaymentCongratsResponse.Score.Progress(0.14f, "#1AC2B0", 2);
+        PaymentCongratsResponse.Score score =
+            new PaymentCongratsResponse.Score(progress, "Sumaste 1 Mercado Punto", action);
+
+        //Payment Methods
+        ArrayList<PaymentInfo> paymentList = new ArrayList();
         paymentList.add(
             new PaymentInfo.Builder()
                 .withPaymentMethodId("account_money")
@@ -43,7 +51,7 @@ public class PaymentCongratsMock {
                 .build()
         );
 
-
+        //Congrats
         PaymentCongratsModel congrats = new PaymentCongratsModel.Builder()
             .withCongratsType(PaymentCongratsModel.CongratsType.APPROVED)
             .withTitle("Payment Congrats Example")
@@ -54,6 +62,7 @@ public class PaymentCongratsMock {
             .withShouldShowReceipt(true)
             .withReceiptId("12312312")
             .withDiscount(discount)
+            .withScore(score)
             .build();
 
         return congrats;
