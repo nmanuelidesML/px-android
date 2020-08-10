0c0dddf8e7c079d47783860fd30ff13b270ad307
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-08-10 11:08:04 -0300

    button on central activity added

--- example/src/main/java/com/mercadopago/CheckoutExampleActivity.java
+++ example/src/main/java/com/mercadopago/CheckoutExampleActivity.java
@@ -9,8 +9,14 @@ import android.widget.Button;
 import com.mercadopago.android.px.core.CheckoutLazyInit;
 import com.mercadopago.android.px.core.MercadoPagoCheckout;
 import com.mercadopago.android.px.feature.custom_initialize.CustomInitializationActivity;
+import com.mercadopago.android.px.internal.features.payment_congrats.PaymentCongrats;
+import com.mercadopago.android.px.internal.features.payment_congrats.model.PaymentCongratsModel;
+import com.mercadopago.android.px.internal.features.payment_congrats.model.PaymentCongratsText;
+import com.mercadopago.android.px.internal.features.payment_congrats.model.PaymentInfo;
 import com.mercadopago.android.px.utils.ExamplesUtils;
 import com.mercadopago.example.R;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import static com.mercadopago.android.px.utils.ExamplesUtils.resolveCheckoutResult;
 
@@ -20,6 +26,7 @@ public class CheckoutExampleActivity extends ExampleBaseActivity {
     private View mRegularLayout;
     private Button continueSimpleCheckout;
     private static final int REQ_CODE_CHECKOUT = 1;
+    private Button paymentCongratsButton;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -64,6 +71,12 @@ public class CheckoutExampleActivity extends ExampleBaseActivity {
 
         continueSimpleCheckout.setOnClickListener(
             v -> ExamplesUtils.createBase().build().startPayment(CheckoutExampleActivity.this, REQUEST_CODE));
+
+        paymentCongratsButton = findViewById(R.id.payment_congrats_button);
+
+        paymentCongratsButton.setOnClickListener(
+            v -> startPaymentCongrats()
+        );
     }
 
     @Override
@@ -80,5 +93,44 @@ public class CheckoutExampleActivity extends ExampleBaseActivity {
 
     private void showRegularLayout() {
         mRegularLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void startPaymentCongrats() {
+
+        ArrayList<PaymentInfo> paymentList = new ArrayList();
+        paymentList.add(
+            new PaymentInfo.Builder()
+                .withPaymentMethodId("account_money")
+                .withPaymentMethodName("Money in Mercado Pago")
+                .withPaymentMethodType(PaymentInfo.PaymentMethodType.ACCOUNT_MONEY)
+                .withAmountPaid("$100")
+                .withDiscountData("50% OFF", "$200")
+                .withDescription(new PaymentCongratsText("Dinero disponible en Mercadopago","","",""))
+                .build()
+        );
+        paymentList.add(
+            new PaymentInfo.Builder()
+                .withPaymentMethodId("master")
+                .withPaymentMethodName("Visa")
+                .withPaymentMethodType(PaymentInfo.PaymentMethodType.CREDIT_CARD)
+                .withLastFourDigits("8020")
+                .withAmountPaid( "$100")
+                .withInstallmentsData(3, "$39,90", "$119,70", BigDecimal.valueOf(19.71))
+                .build()
+        );
+
+
+        PaymentCongratsModel congrats = new PaymentCongratsModel.Builder()
+            .withCongratsType(PaymentCongratsModel.CongratsType.APPROVED)
+            .withTitle("Congrats de la mechi")
+            .withImageUrl("https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
+            .withExitActionSecondary("Dale mechi!", 13)
+            .withPaymentsInfo(paymentList)
+            .withShouldPaymentMethod(true)
+            .withShouldShowReceipt(true)
+            .withReceiptId("12312312")
+            .build();
+
+        PaymentCongrats.show(congrats, this, 13);
     }
 }
